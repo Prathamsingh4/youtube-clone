f58f8832a1016f130d5f26d9e048165061b7a5d4
Pratham Singh <121164027+Prathamsingh4@users.noreply.github.com>
2023-11-22 23:50:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Get the button element by its ID
+var pop = document.getElementById('pop');
+
+// Add a click event listener to the button
+pop.addEventListener('click', function() {
+    // Perform any actions you want here before navigating to the route
+    
+    // Redirect to a specific route (for example, '/newpage')
+    window.location.href = '/register';
+});
